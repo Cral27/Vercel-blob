5eb162c4338ae19343644fcfbc7d5c80fb75caff
unknown <carltan2700@gmail.com>
2024-07-16 13:16:42 +0800

    Changed some text to see if it will reflect on vercel

--- app/page.tsx
+++ app/page.tsx
@@ -9,16 +9,30 @@ export default function AvatarUploadPage() {
 	const [blob, setBlob] = useState<PutBlobResult | null>(null);
 	const [previewUrl, setPreviewUrl] = useState<string | null>(null);
 
+	// const handleFileChange = () => {
+	// 	if(inputFileRef.current?.files){
+	// 		const file = inputFileRef.current.files[0]
+	// 		const reader = new FileReader()
+	// 		reader.onloadend = () => {
+	// 			setPreviewUrl(reader.result as string)
+	// 		}
+	// 		reader.readAsDataURL(file)
+	// 	}
+	// }
+
 	const handleFileChange = () => {
-		if(inputFileRef.current?.files){
-			const file = inputFileRef.current.files[0]
-			const reader = new FileReader()
-			reader.onloadend = () => {
-				setPreviewUrl(reader.result as string)
-			}
-			reader.readAsDataURL(file)
+		console.log('File input changed');
+		if (inputFileRef.current?.files) {
+		  const file = inputFileRef.current.files[0];
+		  console.log('Selected file:', file);
+		  const reader = new FileReader();
+		  reader.onloadend = () => {
+			console.log('FileReader result:', reader.result);
+			setPreviewUrl(reader.result as string);
+		  };
+		  reader.readAsDataURL(file);
 		}
-	}
+	  };
 
 	return (
 		<>
@@ -43,11 +57,12 @@ export default function AvatarUploadPage() {
 				}}
 			>
 				<input name="file" ref={inputFileRef} type="file" onChange={handleFileChange} required />
-				<button type="submit">Upload</button>
+				<button type="submit" className='bg-slate-400 p-2'>Upload</button>
 			</form>
 			{previewUrl && (
 				<div>
-					<img src={previewUrl} alt="Image Preview" className='w-40 h-40'/>
+					{/* <img src={previewUrl} alt="Image Preview" className='w-40 h-40'/> */}
+					
 				</div>
 			)}
 			{blob && (
@@ -55,6 +70,7 @@ export default function AvatarUploadPage() {
 				Blob url: <a href={blob.url}>{blob.url}</a>
 				</div>
 			)}
+			<h1>Should have updated in vercel App</h1>
 		</>
 	);
 }
